abe8717e7cc7b44896cb2212bd7af4595d67059e
olhanemensha <olganemensha1989@gmail.com>
2021-02-28 08:57:53 +0200

    add changes

--- nemensha_olha/src/js/lib/mw.js
+++ nemensha_olha/src/js/lib/mw.js
@@ -1,29 +1,5 @@
 import $ from 'jquery';
 import 'slick-carousel';
-$(document).ready(function($) {
-    $('.popup-open').click(function() {
-        $('.popup-fade').fadeIn();
-        return false;
-    });
-
-    $('.popup-close').click(function() {
-        $(this).parents('.popup-fade').fadeOut();
-        return false;
-    });
-
-    $(document).keydown(function(e) {
-        if (e.keyCode === 27) {
-            e.stopPropagation();
-            $('.popup-fade').fadeOut();
-        }
-    });
-
-    $('.popup-fade').click(function(e) {
-        if ($(e.target).closest('.popup').length == 0) {
-            $(this).fadeOut();
-        }
-    });
-});
 
 $('.container_slider').slick({
     dots: true,
--- nemensha_olha/src/js/lib/sidebar.js
+++ nemensha_olha/src/js/lib/sidebar.js
@@ -2,8 +2,8 @@ window.onscroll = function() {
     scrollFunction()
 };
 
-function scrollFunction() {
+async function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("navbar").style.top = "0";
+        return document.getElementById("navbar").style.top = "0";
     }
 }
